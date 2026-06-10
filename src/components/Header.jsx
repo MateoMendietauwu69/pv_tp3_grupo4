e5a2ebbddce35ffa1c472bd480370a483cf2698b
Iso Mercado <lauty.jose.mercado@gmail.com>
2026-06-10 19:40:28 -0300

    Modificaciones en el Header.jxs Mercado Lautaro

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,26 @@
+import { useContext } from "react" // 
+import { UsuarioContext } from "../context/UsuarioContext" 
 import '../css/Header.css'
-const Header = () =>{
 
-    return (
-        <>
-            <h1>PROYECTOS</h1>
-        </>
-    )
+const Header = () => {
+  const { usuario } = useContext(UsuarioContext)
+
+  return (
+    <header className="main-header" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '10px 20px' }}>
+
+      <h1>PROYECTITOS</h1>
+      
+      <div className="user-info">
+        {usuario ? (
+          <p style={{ margin: 0, fontWeight: 'bold' }}>
+            {usuario.nombre} | <span style={{ fontWeight: 'normal' }}>{usuario.rol}</span>
+          </p>
+        ) : (
+          <p style={{ margin: 0 }}>Invitado</p>
+        )}
+      </div>
+    </header>
+  )
 }
 
 export default Header
